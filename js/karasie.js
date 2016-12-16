c2aff60e64b31f1780106dbe613832e49eb89e06
piotrek <piotr.majcher94@gmail.com>
2016-12-16 22:08:11 +0100

    Kulka zmienia kierunek, ale po kilku zmianach utyka i kręci się w miejscu. Dodano funkcję updateDistFromTarget().

--- js/karasie.js
+++ js/karasie.js
@@ -1,15 +1,47 @@
 var stage = new createjs.Stage("demoCanvas");
 var circle = new createjs.Shape();
 
-function Karas(trgtX, trgtY){
+var naszKaras;
+
+function Karas(trgtX, trgtY, X, Y){
     this.targetX = trgtX;
     this.targetY = trgtY;
+    this.x = X;
+    this.y = Y;
+    this.distFromTarget = updateDistFromTarget(this);
 }
+
+function updateDistFromTarget(ryba) {
+    var vecX = ryba.targetX - ryba.x;
+    var vecY = ryba.targetY  - ryba.y;
+    return Math.sqrt(vecX*vecX + vecY+vecY);
+}
+function getRandomTarget(){
+    var x = Math.random()*stage.canvas.width;
+    var y = Math.random()*stage.canvas.height;
+
+    return [x,y];
+}
+function getMoveVector(ryba){
+    var vectorX = ryba.targetX - ryba.x;
+    var vectorY = ryba.targetY - ryba.y;
+
+    var len = Math.sqrt(vectorX*vectorX + vectorY*vectorY);
+
+    vectorX = vectorX / len;
+    vectorY = vectorY / len;
+
+    return [vectorX, vectorY];
+}
+
 function init(){
 
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5).endFill();
-    circle.x =100;
-    circle.y = 100;
+    var target = getRandomTarget();
+    naszKaras = new Karas(target[0], target[1], 100, 100);
+    circle.x =naszKaras.x;
+    circle.y = naszKaras.y;
+
     stage.addChild(circle);
     stage.update();
     //Update stage will render next frame
@@ -20,24 +52,26 @@ createjs.Ticker.addEventListener("tick", handleTick);
 
 function handleTick(){
     //Circle will move 10 units to the right.
-    var moveVector = getRandMoveVector();
-    circle.x += moveVector[0]*10;
-    circle.y += moveVector[1]*10;
+    var moveVector = getMoveVector(naszKaras);
+    var stepLength = 5;
+
+    naszKaras.x += moveVector[0]*stepLength;
+    naszKaras.y += moveVector[1]*stepLength;
+    naszKaras.distFromTarget = updateDistFromTarget(naszKaras);
+    if(naszKaras.distFromTarget < 4){
+        var newTarget = getRandomTarget();
+        naszKaras.targetX = newTarget[0];
+        naszKaras.targetY = newTarget[1];
+        naszKaras.distFromTarget = updateDistFromTarget(naszKaras);
+    }
+
+    circle.x = naszKaras.x;
+    circle.y = naszKaras.y;
     //Will cause the circle to wrap back
     if (circle.x > stage.canvas.width) { circle.x = 0; }
     if (circle.y > stage.canvas.height) { circle.y = 0; }
     stage.update();
 }
 
-function getRandMoveVector(){
-    var x = Math.random();
-    var y = Math.random();
 
-    var vecLen = Math.sqrt(x*x + y*y);
-
-    x = x/vecLen;
-    y = y/vecLen;
-
-    return [x,y];
-}
 
