121bc9f78af21625b0857545ed8647873697c5a7
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-16 21:21:12 +0100

    test 2

--- js/karasie.js
+++ js/karasie.js
@@ -1,14 +1,29 @@
+var rect;
+var stage;
+
 function init() {
-	var stage = new createjs.Stage("demoCanvas");
 	//Sam obiekt
-	var rect = new createjs.Shape();
+	stage = new createjs.Stage("demoCanvas");
+	rect = new createjs.Shape();
 	//Kolor i wspolrzedne
 	rect.graphics.beginFill("red");
 	rect.graphics.drawRect(100,100,30,20);
 	rect.graphics.endFill();
+
 	//Dodanie do stejdża
 	stage.addChild(rect);
 	//Jakiś apdejcik jeszcze nie
-	stage.update();
-	//xd
+	createjs.Ticker.on("tick", tick);
+	createjs.Ticker.setFPS(30);
+}
+
+function tick(event) {
+	rect.x = rect.x + 5;
+	if(rect.x > stage.canvas.width || rect.x < stage.canvas.width){
+		rect.x = 0;
+	}
+	if(rect.y > stage.canvas.height || rect.y < stage.canvas.height){
+		rect.y = 0;
+	}
+	stage.update(event);
 }
