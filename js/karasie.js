934d41d24e9ba677c2168a621f66a7563c0add3d
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-16 19:49:05 +0100

    delete

--- js/karasie.js
+++ js/karasie.js
@@ -1,9 +1,9 @@
 function init() {
     var stage = new createjs.Stage("demoCanvas");
     var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 10;
-    circle.y = 10;
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5;
+    circle.x = 100;
+    circle.y = 100;
     stage.addChild(circle);
     stage.update();
 }
