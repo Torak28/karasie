a2467846c73adbfdf1a687306f3faf1f6a509b84
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-16 23:02:40 +0100

    zmiany zmiany

--- js/karasie.js
+++ js/karasie.js
@@ -4,71 +4,71 @@ var circle = new createjs.Shape();
 var naszKaras;
 
 function Karas(trgtX, trgtY, X, Y){
-    this.targetX = trgtX;
-    this.targetY = trgtY;
-    this.x = X;
-    this.y = Y;
-    this.distFromTarget = updateDistFromTarget(this);
+	this.targetX = trgtX;
+	this.targetY = trgtY;
+	this.x = X;
+	this.y = Y;
+	this.distFromTarget = function() {
+		var vecX = this.targetX - this.x;
+		var vecY = this.targetY  - this.y;
+		return Math.sqrt(vecX*vecX + vecY+vecY);
+	};
 }
 
-function updateDistFromTarget(ryba) {
-    var vecX = ryba.targetX - ryba.x;
-    var vecY = ryba.targetY  - ryba.y;
-    return Math.sqrt(vecX*vecX + vecY+vecY);
-}
+
 function getRandomTarget(){
-    var x = Math.random()*stage.canvas.width;
-    var y = Math.random()*stage.canvas.height;
+	var x = Math.random()*stage.canvas.width;
+	var y = Math.random()*stage.canvas.height;
 
-    return [x,y];
+	return [x,y];
 }
 function getMoveVector(ryba){
-    var vectorX = ryba.targetX - ryba.x;
-    var vectorY = ryba.targetY - ryba.y;
+	var vectorX = ryba.targetX - ryba.x;
+	var vectorY = ryba.targetY - ryba.y;
 
-    var len = Math.sqrt(vectorX*vectorX + vectorY*vectorY);
+	var len = Math.sqrt(vectorX*vectorX + vectorY*vectorY);
 
-    vectorX = vectorX / len;
-    vectorY = vectorY / len;
+	vectorX = vectorX / len;
+	vectorY = vectorY / len;
 
-    return [vectorX, vectorY];
+	return [vectorX, vectorY];
 }
 
 function init(){
 
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5).endFill();
-    var target = getRandomTarget();
-    naszKaras = new Karas(target[0], target[1], 100, 100);
-    circle.x =naszKaras.x;
-    circle.y = naszKaras.y;
+	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5).endFill();
+	var target = getRandomTarget();
+	naszKaras = new Karas(target[0], target[1], 100, 100);
+	circle.x =naszKaras.x;
+	circle.y = naszKaras.y;
 
-    stage.addChild(circle);
-    stage.update();
-    //Update stage will render next frame
+	stage.addChild(circle);
+	stage.update();
+	//Update stage will render next frame
 
 }
 
 createjs.Ticker.addEventListener("tick", handleTick);
 
 function handleTick(){
-    //Circle will move 10 units to the right.
-    var moveVector = getMoveVector(naszKaras);
-    var stepLength = 5;
+	//Circle will move 10 units to the right.
+	var moveVector = getMoveVector(naszKaras);
+	var stepLength = 5;
 
-    naszKaras.x += moveVector[0]*stepLength;
-    naszKaras.y += moveVector[1]*stepLength;
-    naszKaras.distFromTarget = updateDistFromTarget(naszKaras);
-    if(naszKaras.distFromTarget < 4){
-        var newTarget = getRandomTarget();
-        naszKaras.targetX = newTarget[0];
-        naszKaras.targetY = newTarget[1];
-        naszKaras.distFromTarget = updateDistFromTarget(naszKaras);
-    }
+	circle.x = naszKaras.x;
+	circle.y = naszKaras.y;
 
-    circle.x = naszKaras.x;
-    circle.y = naszKaras.y;
-    //Will cause the circle to wrap back
-    if (circle.x > stage.canvas.width) { circle.x = 0; }
-    if (circle.y > stage.canvas.height) { circle.y = 0; }
-    stage.update();
+	naszKaras.x += moveVector[0]*stepLength;
+	naszKaras.y += moveVector[1]*stepLength;
+
+
+	var dist = naszKaras.distFromTarget();
+
+	if(dist < 3.0) {
+		var newTarget = getRandomTarget();
+		naszKaras.targetX = newTarget[0];
+		naszKaras.targetY = newTarget[1];
+	}
+
+	stage.update();
 }
