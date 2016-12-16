0ed538b5f1ee1fd61844c7587f1ce0ffbb3472ff
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-17 00:11:51 +0100

    Podzial na pliczki

---
+++ js/Karas.js
@@ -0,0 +1,50 @@
+function Karas(trgtX, trgtY, X, Y){
+    this.karasShape = new createjs.Shape();
+    this.karasShape.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5).endFill();
+    this.karasShape.x = X;
+    this.karasShape.y = Y;
+
+    this.targetX = trgtX;
+    this.targetY = trgtY;
+    this.distFromTarget = function() {
+        var vecX = this.targetX - this.karasShape.x;
+        var vecY = this.targetY  - this.karasShape.y;
+        return Math.sqrt(vecX*vecX + vecY+vecY);
+    };
+
+    this.updatePosition = function (steps) {
+        var moveVector = this.getMoveVector();
+        this.karasShape.x += moveVector[0]*steps;
+        this.karasShape.y += moveVector[1]*steps;
+
+        var dist = this.distFromTarget();
+
+        while(dist < 5.0){
+            var newTarget = getRandomTarget();
+            this.targetX = newTarget[0];
+            this.targetY = newTarget[1];
+            dist = this.distFromTarget();
+        }
+
+    }
+
+    this.getMoveVector = function (){
+        var vectorX = this.targetX - this.karasShape.x;
+        var vectorY = this.targetY - this.karasShape.y;
+
+        var len = Math.sqrt(vectorX*vectorX + vectorY*vectorY);
+        console.log("length" + len);
+        vectorX = vectorX / len;
+        vectorY = vectorY / len;
+        console.log("vectorX " + vectorX + " vectorY " + vectorY )
+
+        return [vectorX, vectorY];
+    }
+}
+
+function getRandomTarget(){
+    var x = Math.random()*stage.canvas.width;
+    var y = Math.random()*stage.canvas.height;
+
+    return [x,y];
+}
--- js/karasie.js
+++ js/karasie.js
@@ -1,74 +1,23 @@
 var stage = new createjs.Stage("demoCanvas");
-var circle = new createjs.Shape();
 
-var naszKaras;
-
-function Karas(trgtX, trgtY, X, Y){
-	this.targetX = trgtX;
-	this.targetY = trgtY;
-	this.x = X;
-	this.y = Y;
-	this.distFromTarget = function() {
-		var vecX = this.targetX - this.x;
-		var vecY = this.targetY  - this.y;
-		return Math.sqrt(vecX*vecX + vecY+vecY);
-	};
-}
-
-
-function getRandomTarget(){
-	var x = Math.random()*stage.canvas.width;
-	var y = Math.random()*stage.canvas.height;
-
-	return [x,y];
-}
-function getMoveVector(ryba){
-	var vectorX = ryba.targetX - ryba.x;
-	var vectorY = ryba.targetY - ryba.y;
-
-	var len = Math.sqrt(vectorX*vectorX + vectorY*vectorY);
-
-	vectorX = vectorX / len;
-	vectorY = vectorY / len;
-
-	return [vectorX, vectorY];
-}
+var count = 10;
+var karasie = new Family(count);
 
 function init(){
 
-	circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5).endFill();
-	var target = getRandomTarget();
-	naszKaras = new Karas(target[0], target[1], 100, 100);
-	circle.x =naszKaras.x;
-	circle.y = naszKaras.y;
+    for(var i=0; i< count; i++){
+        stage.addChild(karasie.family[i].karasShape);
+    }
 
-	stage.addChild(circle);
-	stage.update();
-	//Update stage will render next frame
-
+    stage.update();
+    //Update stage will render next frame
 }
 
 createjs.Ticker.addEventListener("tick", handleTick);
 
 function handleTick(){
-	//Circle will move 10 units to the right.
-	var moveVector = getMoveVector(naszKaras);
-	var stepLength = 5;
+    //Circle will move 10 units to the right.
 
-	circle.x = naszKaras.x;
-	circle.y = naszKaras.y;
-
-	naszKaras.x += moveVector[0]*stepLength;
-	naszKaras.y += moveVector[1]*stepLength;
-
-
-	var dist = naszKaras.distFromTarget();
-
-	if(dist < 3.0) {
-		var newTarget = getRandomTarget();
-		naszKaras.targetX = newTarget[0];
-		naszKaras.targetY = newTarget[1];
-	}
-
-	stage.update();
+    karasie.updateFamily(5);
+    stage.update();
 }
