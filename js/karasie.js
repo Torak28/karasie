688da90678da549d0fd4bc082028730c2973b5d8
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-16 20:39:42 +0100

    Kwadraty, fix

--- js/karasie.js
+++ js/karasie.js
@@ -1,6 +1,5 @@
 function init() {
-	var canvas = document.getElementById("Canvas");
-	var stage = new createjs.Stage(canvas);
+	var stage = new createjs.Stage("demoCanvas");
 
 	//Sam obiekt
 	var rect = new createjs.Graphics();
