7e880a40c81a14843597ce06f99c8fb8b8069f78
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-16 19:47:51 +0100

    test 10

--- js/karasie.js
+++ js/karasie.js
@@ -1,13 +1,9 @@
-/**
- * Created by wojak on 16.12.16.
- */
-
 function init() {
     var stage = new createjs.Stage("demoCanvas");
     var circle = new createjs.Shape();
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 50);
-    circle.x = 100;
-    circle.y = 100;
+    circle.x = 10;
+    circle.y = 10;
     stage.addChild(circle);
     stage.update();
 }
