4c89a9b079c5ece73a7df3b68c462ccb5f54f885
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-16 20:15:44 +0100

    rect

--- js/karasie.js
+++ js/karasie.js
@@ -1,13 +1,14 @@
 function init() {
-    var canvas = document.getElementById("Canvas");
-    var stage = new createjs.Stage(canvas);
+	var canvas = document.getElementById("Canvas");
+	var stage = new createjs.Stage(canvas);
 
-    //Sam obiekt
-    var rect = new createjs.Shape();
-    //Kolor i wspolrzedne
-    rect.graphics.beginFill("DeepSkyBlue").drawRect(20,20,100,50);
-    //Dodanie do stejdża
-    stage.addChild(rect);
-    //Jakiś apdejcik jeszcze nie
-    stage.update();
+	//Sam obiekt
+	var rect = new createjs.Graphics();
+	//Kolor i wspolrzedne
+	rect.graphics.beginFill("red");
+	rect.drawRect(20,20,100,50);
+	//Dodanie do stejdża
+	stage.addChild(rect);
+	//Jakiś apdejcik jeszcze nie
+	stage.update();
 }
