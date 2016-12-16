4051919e7e6ef03e3c7e9e33a3bfec525d87aa7c
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-16 20:06:26 +0100

    Komentarz do kodu

--- js/karasie.js
+++ js/karasie.js
@@ -1,9 +1,18 @@
 function init() {
-    var stage = new createjs.Stage("demoCanvas");
+    var canvas = document.getElementById("Canvas");
+    var stage = new createjs.Stage(canvas);
+
+    //Sam obiekt
     var circle = new createjs.Shape();
+    //Kolor i wspolrzedne
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5);
+    //Nadpisanie XY
     circle.x = 100;
     circle.y = 100;
+    //Dodanie do stejdża
     stage.addChild(circle);
+
+
+    //Jakiś apdejcik jeszcze nie wiem co
     stage.update();
 }
