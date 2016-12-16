1611df01071c25bf02c7882cb39a0e9b0f3d36b0
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2016-12-16 21:30:47 +0100

    Piotrek, Piotrek, Piotrek

--- js/karasie.js
+++ js/karasie.js
@@ -1,29 +1,42 @@
-var rect;
-var stage;
+var stage = new createjs.Stage("demoCanvas");
+var circle = new createjs.Shape();
 
-function init() {
-	//Sam obiekt
-	stage = new createjs.Stage("demoCanvas");
-	rect = new createjs.Shape();
-	//Kolor i wspolrzedne
-	rect.graphics.beginFill("red");
-	rect.graphics.drawRect(100,100,30,20);
-	rect.graphics.endFill();
+function Karas(trgtX, trgtY){
+    this.targetX = trgtX;
+    this.targetY = trgtY;
+}
+function init(){
 
-	//Dodanie do stejdża
-	stage.addChild(rect);
-	//Jakiś apdejcik jeszcze nie
-	createjs.Ticker.on("tick", tick);
-	createjs.Ticker.setFPS(30);
+    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 5).endFill();
+    circle.x =100;
+    circle.y = 100;
+    stage.addChild(circle);
+    stage.update();
+    //Update stage will render next frame
+
 }
 
-function tick(event) {
-	rect.x = rect.x + 5;
-	if(rect.x > stage.canvas.width || rect.x < stage.canvas.width){
-		rect.x = 0;
-	}
-	if(rect.y > stage.canvas.height || rect.y < stage.canvas.height){
-		rect.y = 0;
-	}
-	stage.update(event);
+createjs.Ticker.addEventListener("tick", handleTick);
+
+function handleTick(){
+    //Circle will move 10 units to the right.
+    var moveVector = getRandMoveVector();
+    circle.x += moveVector[0]*10;
+    circle.y += moveVector[1]*10;
+    //Will cause the circle to wrap back
+    if (circle.x > stage.canvas.width) { circle.x = 0; }
+    if (circle.y > stage.canvas.height) { circle.y = 0; }
+    stage.update();
+}
+
+function getRandMoveVector(){
+    var x = Math.random();
+    var y = Math.random();
+
+    var vecLen = Math.sqrt(x*x + y*y);
+
+    x = x/vecLen;
+    y = y/vecLen;
+
+    return [x,y];
 }
