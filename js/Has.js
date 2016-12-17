5107a1621008cbaf534ac8a0db644bbcdec482d6
Wojak94 <wojciechadles@gmail.com>
2016-12-17 08:26:24 +0100

    merge

---
+++ js/Has.js
@@ -0,0 +1,12 @@
+/**
+ * Created by piotrek on 17.12.16.
+ */
+function Has(X,Y){
+	this.hasShape = new createjs.Shape();
+	this.hasShape.graphics.beginFill("red").drawCircle(0, 0, 5).endFill();
+	this.hasShape.x = X;
+	this.hasShape.y = Y;
+	this.dodatek = 50;
+	this.id = 0;
+	this.zjedzony = false;
+}
--- js/karasie.js
+++ js/karasie.js
@@ -1,14 +1,92 @@
-function init() {
-	var canvas = document.getElementById("Canvas");
-	var stage = new createjs.Stage(canvas);
+var stage = new createjs.Stage("demoCanvas");
 
-	//Sam obiekt
-	var rect = new createjs.Graphics();
-	//Kolor i wspolrzedne
-	rect.beginFill("red");
-	rect.drawRect(20,20,100,50);
-	//Dodanie do stejdża
-	//stage.addChild(rect);
-	//Jakiś apdejcik jeszcze nie
-	//stage.update();
+var count = 10;
+var karasie = new Family(count);
+var iloscJedzonka = 5;
+var hasie = new Hasie();
+
+function init(){
+
+	for(var i=0; i< count; i++){
+		stage.addChild(karasie.family[i].karasShape);
+	}
+	for(var i=0; i< iloscJedzonka; i++){
+		stage.addChild(hasie.family[i].hasShape);
+		hasie.family[i].id = count+i;
+	}
+
+	spawnHas();
+	stage.update();
+	//Update stage will render next frame
 }
+
+createjs.Ticker.addEventListener("tick", handleTick);
+
+function handleTick(){
+	//Circle will move 10 units to the right.
+
+	karasie.updateFamily(5);
+	stage.update();
+}
+
+function checkCollisions(karas) {
+
+	for(var i=0; i<count; i++){
+		if(!karasie.family[i].alive || karasie.family[i].id == karas.id )
+			continue;
+
+		var dx = karas.karasShape.x - karasie.family[i].karasShape.x;
+		var dy = karas.karasShape.y - karasie.family[i].karasShape.y;
+		var dist = Math.sqrt(dx*dx + dy*dy);
+
+		if(dist<=10.0){
+			//console.log("distance " + dist);
+			//console.log("Zderzenie!");
+			return true;
+		}
+
+
+	}
+	return false;
+}
+
+function findHasie(karas) {
+
+	for(var i=0; i<iloscJedzonka; i++){
+		if(hasie.family[i].zjedzony)
+			continue;
+		var dx = karas.karasShape.x - hasie.family[i].hasShape.x;
+		var dy = karas.karasShape.y - hasie.family[i].hasShape.y;
+		var dist = Math.sqrt(dx*dx + dy*dy);
+
+		if(dist<=50.0)
+			return [hasie.family[i].hasShape.x, hasie.family[i].hasShape.y ];
+
+	}
+	return [-1,-1];
+}
+
+function eatHas(karas) {
+
+	for(var i=0; i<iloscJedzonka; i++){
+		var dx = karas.karasShape.x - hasie.family[i].hasShape.x;
+		var dy = karas.karasShape.y - hasie.family[i].hasShape.y;
+		var dist = Math.sqrt(dx*dx + dy*dy);
+
+		if(dist<=3.0) {
+			hasie.family[i].zjedzony = true;
+			hasie.updateHasie();
+			karas.ticksToDeath += hasie.family[i].dodatek;
+
+			return true;
+		}
+
+	}
+	return false;
+}
+
+function spawnHas() {
+	console.log("spawn");
+	hasie.addHas();
+}
+
